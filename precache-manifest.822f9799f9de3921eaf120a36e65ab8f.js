422dd6b2b994851f569badd6f9bfa40756100b20
Dennis Ent <dennisent@Denniss-MacBook-Pro.local>
2019-04-03 19:15:15 +1100

    Updates

---
+++ precache-manifest.822f9799f9de3921eaf120a36e65ab8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ca30ceeff78871a11a4",
+    "url": "/lottery-ethereum/static/css/main.c19cf48a.chunk.css"
+  },
+  {
+    "revision": "2ca30ceeff78871a11a4",
+    "url": "/lottery-ethereum/static/js/main.b700c7a1.chunk.js"
+  },
+  {
+    "revision": "ce739fe4f2561451cd22",
+    "url": "/lottery-ethereum/static/js/runtime~main.667ccf24.js"
+  },
+  {
+    "revision": "a74edf973b0ca9c70d6b",
+    "url": "/lottery-ethereum/static/js/2.6b876d9d.chunk.js"
+  },
+  {
+    "revision": "ef7e06781c233745a41a20ea3dcaa0e9",
+    "url": "/lottery-ethereum/index.html"
+  }
+];
